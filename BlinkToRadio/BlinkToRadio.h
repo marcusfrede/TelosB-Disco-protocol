3fbe223221bad394fb96809d577f50a2768cadcc
marcusfrede <marcus.frederiksen@gmail.com>
2017-05-10 05:35:34 -0700

    initial

---
+++ BlinkToRadio/BlinkToRadio.h
@@ -0,0 +1,32 @@
+// $Id: BlinkToRadio.h,v 1.4 2006/12/12 18:22:52 vlahan Exp $
+
+#ifndef BLINKTORADIO_H
+#define BLINKTORADIO_H
+
+enum {
+  AM_BLINKTORADIO = 6,
+  TIMER_PERIOD_MILLI = 250
+  };
+
+typedef nx_struct BlinkToRadioMsg {
+  nx_uint16_t nodeid;
+  nx_uint16_t counter;
+} BlinkToRadioMsg;
+
+#endif
+
+
+/**
+ * Implementation of the BlinkToRadio application.  A counter is
+ * incremented and a radio message is sent whenever a timer fires.
+ * Whenever a radio message is received, the three least significant
+ * bits of the counter in the message payload are displayed on the
+ * LEDs.  Program two motes with this application.  As long as they
+ * are both within range of each other, the LEDs on both will keep
+ * changing.  If the LEDs on one (or both) of the nodes stops changing
+ * and hold steady, then that node is no longer receiving any messages
+ * from the other node.
+ *
+ * @author Prabal Dutta
+ * @date   Feb 1, 2006
+ */
